eb70150715645051eff174a18d8f035185a379d3
Marcela Fernandes <mam.fernandes@hotmail.com>
2023-02-09 13:31:13 -0700

    W01D4 - findWaldo Part2

--- Lighthouse/focal/functions/callbacks-intro/findWaldo.js
+++ Lighthouse/focal/functions/callbacks-intro/findWaldo.js
@@ -1,14 +1,13 @@
 const findWaldo = function(names, found) {
-  for (let i = 0; i < names.length; i++) {
-    let waldoName = names[i];
-    if (waldoName === "Waldo") {
-      found(names.indexOf(waldoName))   // execute callback
+  names.forEach((element, index) => {
+    if (element === "Waldo") {
+      found(index); // execute callback
     }
-  }
-}
+  });
+};
 
 const actionWhenFound = function(index) {
   console.log(`Found Waldo at index ${index}!`);
-}
+};
 
-findWaldo(["Alice", "Bob", "john", "Waldo", "Winston"], actionWhenFound);
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
