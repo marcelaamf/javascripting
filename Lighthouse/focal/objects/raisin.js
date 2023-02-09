670d93862239cb8f33d826e4fe58e07c0f9bc5d4
Marcela Fernandes <mam.fernandes@hotmail.com>
2023-02-09 09:51:34 -0700

    W01D03 -  pair programming -  raisin assignment

---
+++ Lighthouse/focal/objects/raisin.js
@@ -0,0 +1,13 @@
+const raisinAlarm = function(cookie) {
+  // Put your solution here
+  for (let i = 0; i < cookie.length; i++) {
+    if (cookie[i] === "🍇") {
+      return "Raisin alert!";
+    }
+  }
+  return "All good!";
+};
+
+console.log(raisinAlarm(["🍫", "🍫", "🍇", "🍫"]));
+console.log(raisinAlarm(["🍫", "🍇", "🍫", "🍫", "🍇"]));
+console.log(raisinAlarm(["🍫", "🍫", "🍫"]));
